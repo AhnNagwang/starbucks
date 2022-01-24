0ea0fd61b57a90427a7b6ed07afa46ef4d24af38
AhnNaGwang <ahnnagwang@naver.com>
2022-01-24 22:45:44 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function ()
+{console. log('scroll!');
+if (window.scrollY > 500) {
+  // 배지 숨기기
+  gsap.to (badgeEl, .6, {
+    opacity: 0,
+    display: 'none'
+  });
+}else {
+  gsap.to (badgeEl, .6, {
+    opacity: 1,
+    display: 'block'
+    
+  });
+}
+
+}, 300));
+// _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index +1)* .7,
+    opacity: 1
+  });
+});
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 5000
+  // }
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promtion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+      promotionEl.classList.add('hide');
+  }else {
+      promotionEl.classList.remove('hide');
+  }
+});
